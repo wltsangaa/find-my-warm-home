9a6f3884ed0be787385be437a4ccd93168a328c8
Keith Chan <keithchan@Keiths-MacBook-Pro.local>
2019-01-18 14:53:57 +0800

    Keith home version

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-apiKey: "AIzaSyCDv-RIo0QNfiSXu_X8boZ4j4XOzy69X-c",
-    authDomain: "chatrecord-9c992.firebaseapp.com",
-    databaseURL: "https://chatrecord-9c992.firebaseio.com",
-    projectId: "chatrecord-9c992",
-    storageBucket: "chatrecord-9c992.appspot.com",
-    messagingSenderId: "805974697583"
+    apiKey: "AIzaSyD7vlkMftKX99JYV4ojcG74g6if8JxVZUk",
+    authDomain: "findhome-217708.firebaseapp.com",
+    databaseURL: "https://findhome-217708.firebaseio.com",
+    projectId: "findhome-217708",
+    storageBucket: "findhome-217708.appspot.com",
+    messagingSenderId: "459572326769"
 };
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,41 +6,84 @@ import { AuthService } from '../services/auth.service';
 import { SignupPage } from '../signup/signup';
 //import * as firebase from 'Firebase';
 import 'firebase/firestore';
-import { AngularFirestore }  from '@angular/fire/firestore';
+//import { AngularFirestore }  from '@angular/fire/firestore';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import 'rxjs/add/operator/map';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { LoginPage } from '../login/login';
+import { DatePipe } from '@angular/common';
+
+interface Property {
+  displayText: string;
+  price: number;
+  totalFloors: number;
+}
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 	email:string;
   password:string;
   
+  
   //ref = firebase.database().ref('userp/');
   hisdata:Observable<any[]>;
-  items: Observable<any[]>;
+  items:any;
+  propertysCol: AngularFirestoreCollection<Property>;
+  properties: Observable<Property[]>;
   
   constructor(
     //public db: AngularFireDatabase,
+    public db: AngularFirestore,
     public navCtrl: NavController,
-  public authService: AuthService,
-  public fireStore: AngularFirestore
+    public authService: AuthService,
   ) {
-    //this.items = db.list('chatrooms').valueChanges();
-    
-
-
-    
+    //this.booksCollection = this.db.collection('historical_price');
   }
 
   ionViewDidLoad(){
-    this.hisdata = this.fireStore.collection<any>('historical_price', ref=>
-    { ref.where("displaytext", "==", "hello");
-    return ref;
-  }).valueChanges();
-  console.log(this.hisdata);
+    // this.hisdata = this.db.collection('historical_price').valueChanges();
+    // this.hisdata = this.db.collection<any>
+    // ('historical_price', ref=>ref.where("totalFloors", "==", "1")).valueChanges();
+    //this.hisdata = this.db.collection('historical_price', ref => ref.orderBy('price', 'desc').limit(5)).valueChanges();
+    //this.hisdata = this.db.collection('historical_price', ref => ref.where("totalFloors", "==", "1")).valueChanges();
+    //getPropertyList();
+    //this.propertysCol = this.db.collection('historical_price');
+    let floor:number = 1;
+    
+    // testing
+    // this.propertysCol = this.db.collection('historical_price', ref => ref.where('totalFloors', '==', floor));
+
+    // search filter -- timestamp(desc)
+    this.propertysCol = this.db.collection('historical_price', ref => ref.orderBy('price', 'desc').limit(2));
+
+    this.properties = this.propertysCol.valueChanges();
   }
+
+getPropertyList(){
+  return this.hisdata = this.db.collection('historical_price', ref => ref.where("totalFloors", "==", "1")).valueChanges();
+}
+
+convert2Date(time:any){
+  let newDate = new Date(+time);
+  //const myFormattedDate = newDate.toDateString();
+  return JSON.stringify(newDate);
+  // 636736255693017525
+  // 636724110235344872
+}
+
+postTags(postTagsArray:any){
+  for (let i = 0; i < postTagsArray.length; i++){
+    if(postTagsArray[i].includes("校網") || postTagsArray[i].includes("indoor")){
+      postTagsArray.splice(i, 1);
+    }
+  }
+  return postTagsArray;
+}
 
 signup() {
   this.navCtrl.push(SignupPage);
